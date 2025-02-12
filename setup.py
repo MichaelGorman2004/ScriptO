be2ae7fcfb628f64f30b2512e2ea5e91b6a265e6
MichaelGorman2004 <gorman.270@osu.edu>
2025-02-11 21:29:57 -0500

    fix/finalizing for the night

--- setup.py
+++ setup.py
@@ -4,7 +4,10 @@ setup(
     name="scripto",
     version="1.0.0",
     packages=find_packages(),
+    python_requires=">=3.8",
     install_requires=[
-        # Your requirements will be read from requirements.txt
+        line.strip()
+        for line in open("requirements.txt")
+        if line.strip() and not line.startswith("#")
     ],
 ) 
